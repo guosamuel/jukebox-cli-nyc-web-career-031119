15e3072d52ec63e248fc942cd454774a392053e0
Samuel Guo <guosamuel1114@gmail.com>
2019-03-05 14:31:57 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -34,7 +34,7 @@ def play(songs_list)
   puts "Please enter a song name or number:"
   user_input = gets.chomp
   if (user_input.to_i < 10 && user_input.to_i > 0)
-    puts "Playing #{songs_list[user_input]}"
+    puts "Playing #{songs_list[user_input.to_i]}"
   elsif
     songs_list.each do |song|
       if user_input == song
