19481db3a523c0feac0ab557045c9dceb86876ef
Samuel Guo <guosamuel1114@gmail.com>
2019-03-05 14:42:11 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -34,8 +34,8 @@ end
 
 def play(songs_list)
   puts "Please enter a song name or number:"
-  user_input = gets.chomp
   binding.pry
+  user_input = gets.chomp
   if (user_input.to_i < 10 && user_input.to_i > 0)
     puts "Playing #{songs_list[user_input.to_i]}"
   elsif !(user_input.to_i < 10 && user_input.to_i > 0)
