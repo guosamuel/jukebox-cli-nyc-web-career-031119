2580cee727e9a498cc6900cd4387bc5f544633ad
Samuel Guo <guosamuel1114@gmail.com>
2019-03-05 14:25:02 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,28 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts  "I accept the following commends:"
+  puts  " - help : displays this help message"
+  puts  " - list : displays a list of songs you can play"
+  puts  " - play : lets your choose a song to play"
+  puts  " - exit : exits this program"
+end
+
+def list(list_of_songs)
+  counter = 0
+  while counter < list_of_songs.length
+    puts "#{counter+1}. #{list_of_songs[counter]}"
+    counter += 1
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def play(songs_list)
+  puts "Please enter a song name or number:"
+  user_input = gets.chomp
+  if (user_input < 10 && user_input > 0)
+end
